90eadeb31ec363f79ceff1ee2f07d3c7c8eed0fd
teekamsuthar <teekam.suthar1@gmail.com>
2025-04-27 17:14:08 +0530

    add autostart file

---
+++ start-fiverr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd /home/pi/your-extension-folder
+web-ext run \
+  --firefox-profile="zjigiz0vq.default-esr" \
+  --keep-profile-changes \
+  --start-url="https://www.fiverr.com/seller_dashboard"
+
+# chmod +x start-fiverr.sh to make it executable
+# Method 1 LXDE autostart file
+# Create the autostart folder (if it doesn’t exist already):
+
+# bash
+# mkdir -p ~/.config/lxsession/LXDE-pi
+# Edit (or create) the autostart file:
+
+# bash
+# nano ~/.config/lxsession/LXDE-pi/autostart
+# Add this line at the end (replace with your actual path):
+
+
+# @/home/pi/start-fiverr.sh
+# Save (Ctrl + O, Enter) and exit (Ctrl + X).
+
+# On your next boot (and auto-login into the desktop), your script will run automatically.
